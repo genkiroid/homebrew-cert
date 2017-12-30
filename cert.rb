35a735a2fb714580bd757f3c7f6830ec008e2202
genkiroid <gamurai@gmail.com>
2017-12-30 12:16:14 +0900

    0.7.1

--- cert.rb
+++ cert.rb
@@ -1,14 +1,14 @@
 class Cert < Formula
   desc "Get server's certificate information."
   homepage "https://github.com/genkiroid/cert"
-  version "0.7.0"
+  version "0.7.1"
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/genkiroid/cert/releases/download/0.7.0/cert_0.7.0_macOs_64bit.tar.gz"
-    sha256 "1b7e7807e8a027fd429e3f3ee471526a5a65d08c1491cf571cb76cc2439156d3"
+    url "https://github.com/genkiroid/cert/releases/download/0.7.1/cert_0.7.1_macOs_64bit.tar.gz"
+    sha256 "4e80f477c437ed770990c3395d53b80eaf9aef485c34bb9360423c5e39047efe"
   else
-    url "https://github.com/genkiroid/cert/releases/download/0.7.0/cert_0.7.0_macOs_32bit.tar.gz"
-    sha256 "b3f2777c798f39cdce279ad835276c12acab078590ccc1dcbd8d7d4a95fa00db"
+    url "https://github.com/genkiroid/cert/releases/download/0.7.1/cert_0.7.1_macOs_32bit.tar.gz"
+    sha256 "82a34a21138066ed9e770c03535cd95566f75949e55cf5873edccf44f65c0028"
   end
 
   def install
