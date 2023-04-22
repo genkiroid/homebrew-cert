39b9d3b3bdfe91812e26db3650a5529ea19d8650
genkiroid <gamurai@gmail.com>
2023-04-22 09:28:49 +0900

    1.0.0

--- cert.rb
+++ cert.rb
@@ -1,10 +1,10 @@
 class Cert < Formula
   desc "Get server's certificate information."
   homepage "https://github.com/genkiroid/cert"
-  version "0.16.0"
+  version "1.0.0"
 
-  url "https://github.com/genkiroid/cert/releases/download/0.16.0/cert_0.16.0_macOs_64bit.tar.gz"
-  sha256 "7e27679248646b83ff438e841a718e9329520f8894b9ee3fc05e4215199a42c1"
+  url "https://github.com/genkiroid/cert/releases/download/1.0.0/cert_1.0.0_macOs_64bit.tar.gz"
+  sha256 "2f6223f9839efa3c93b279db8a05f130cf9d971d8bfc5acf1c2dbf749de78481"
 
   def install
     bin.install "cert"
